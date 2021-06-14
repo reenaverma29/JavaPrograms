aa2ecc1e5bdd2bc507e6e78fae7560166b33658f
Reena Verma <84978397+reenaverma29@users.noreply.github.com>
2021-06-14 12:54:08 +0000

    Added Welcome & Example2 File

---
+++ Example2.java
@@ -0,0 +1,10 @@
+class Example2 {
+  public static void main (String args[]){
+    int a;// declare a variable named a
+    a=100; // assigning the value to variable a 
+    System.out.print("This is num:" + a);
+    a = a * 2;
+    System.out.print("The value of a * 2 is");
+    System.out.println(a);
+  }
+}
--- Welcome.java
+++ Welcome.java
@@ -2,7 +2,7 @@ class Welcome
 {
   public static void main (String args[])
   {
-    System.out.print("Welcome to Java");
+    System.out.printl("Welcome to Java");
     System.out.println("Welcome to Java");
   }
 }
