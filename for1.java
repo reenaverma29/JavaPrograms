127d08d8eb778af03b69dc1cbaad0c0600c2b796
Reena Verma <84978397+reenaverma29@users.noreply.github.com>
2021-06-28 13:31:49 +0000

    Added new for File

---
+++ for1.java
@@ -0,0 +1,7 @@
+class for1 {
+  public static void main(String args[]){
+    int a;
+    for(a=5;a>=0;a--)
+    System.out.println("This is the value of a:" + a);
+  }
+}
--- forExample.java
+++ forExample.java
@@ -1,6 +1,6 @@
 class forExample {
   public static void main (String args[]){
-    int x;
+    int x;//declaring
     for (x=0; x<10; x++)
     System.out.println("\nThis is x:" + x);
   }
